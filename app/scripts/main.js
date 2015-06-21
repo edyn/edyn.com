f1b8e5d5931afa3eb6a631f3767a3f578edd9ffa
Ahmed Chergaoui <chergaoui@gmail.com>
2015-06-21 12:03:01 +0000

    Adds hero unit
    Adds navigation for mobile
    Adds basic animation for the hamburger menu icon
    Adds icon webfont

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+(function(){
+	'use strict';
+
+	var headerMenuList = $('.menu-header ul');
+	var footerMenuElements = $('.menu-footer li').clone();
+
+	$('.menu-icon').click(function() {
+		headerMenuList.append(footerMenuElements);
+		$(this).toggleClass('is-open');
+	});
+
+})();
