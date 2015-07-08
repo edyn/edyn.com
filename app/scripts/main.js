8f8f5df415a67d3e3b1d1396e364a8a63e56f429
Ahmed Chergaoui <chergaoui@gmail.com>
2015-07-08 04:22:41 +0000

    Multiple bugfixes according to the QA_07022015
    - Fixes transitions bugs
    - Fixes text font sizes & weights
    - Fixes carousel transition & images
    - Changes copy
    - Adds new transitions
    - Fixes logo changing events

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,7 @@
 
 	// Whatever happens for resolutions under 47.5em
 	if (window.matchMedia('(max-width: 47.5em)').matches) {
-		// Fixing the jumpy background image due to "vh" unit
+		// Fixing the jumpy background image due to 'vh' unit
 		// var heroBg = jQuery('.hero-background');
 		// var h = window.innerHeight;
 		// heroBg.css('height', h);
@@ -15,6 +15,7 @@
 			jQuery('.menu-icon').click(function() {
 				headerMenuList.append(footerMenuElements);
 				jQuery(this).toggleClass('is-open');
+				jQuery('body').toggleClass('no-scroll');
 			});
 		//--- END mobile hamburger menu
 
@@ -46,7 +47,8 @@
 			itemsDesktop: [1000, 3],
 			itemsDesktopSmall: [900, 3],
 			itemsTablet: [600, 1],
-			itemsMobile: [480, 1]
+			itemsMobile: [480, 1],
+			transitionStyle: 'fade'
 		});
 		//--- END carousel
 	}
@@ -62,7 +64,9 @@
 			itemsDesktop: [1000, 1],
 			itemsDesktopSmall: [900, 1],
 			itemsTablet: [600, 1],
-			itemsMobile: [480, 1]
+			itemsMobile: [480, 1],
+			singleItem: true,
+			transitionStyle: 'fade'
 		});
 
 		jQuery('.hero-content .carousel').owlCarousel({
@@ -76,8 +80,9 @@
 		});
 		//--- END carousel
 
-		jQuery('.hero-background').attr('data-0p-top-top', 'opacity: 1;');
-		jQuery('.hero-background').attr('data--10p-top-top', 'opacity: 0;');
+		// jQuery('.hero-background').attr('data-0p-bottom-bottom', 'opacity: 1;');
+		// jQuery('.hero-background').attr('data--70p-bottom-bottom', 'opacity: 0;');
+		// jQuery('.hero-background').attr('data-anchor-target', '.hero-content');
 
 		jQuery('.section-sensor .section-item:first').attr('data-0-bottom-bottom', 'opacity: 0; position: relative; top: 40vh;');
 		jQuery('.section-sensor .section-item:first').attr('data--20p-bottom-bottom', 'opacity: 1; position: relative; top: 20vh;');
@@ -89,7 +94,6 @@
 		jQuery('.section-sensor .section-item').eq(1).attr('data-0p-top-top', 'opacity: 1; position: relative; top: 0vh;');
 		jQuery('.section-sensor .section-item').eq(1).attr('data--20p-top-top', 'opacity: 0; position: relative; top: -20vh;');
 
-		jQuery('.section-sensor').attr('data-80p-top-top', 'background-position: 10% 100vh;');
 		jQuery('.section-sensor').attr('data-20p-top-top', 'background-position: 10% 30vh; background-attachment: fixed;');
 		jQuery('.section-sensor').attr('data--80p-top-top', 'background-position: 10% 15vh; background-attachment: fixed;');
 		jQuery('.section-sensor').attr('data--120p-top-top', 'background-position: 10% 0vh; ');
@@ -129,20 +133,68 @@
 		jQuery('.section-app .carousel-item').eq(2).find('img').attr('data-0p-top-top', 'opacity: 1;');
 		jQuery('.section-app .carousel-item').eq(2).find('img').attr('data--10p-top-top', 'opacity: 0;');
 		jQuery('.section-app .carousel-item').eq(2).find('img').attr('data-anchor-target', '.carousel-item-3');
+
+		jQuery('.section-app .carousel-item').eq(3).find('.carousel-content').attr('data-0p-bottom-bottom', 'opacity: 0;');
+		jQuery('.section-app .carousel-item').eq(3).find('.carousel-content').attr('data--20p-bottom-bottom', 'opacity: 1;');
+		jQuery('.section-app .carousel-item').eq(3).find('.carousel-content').attr('data-0p-top-top', 'opacity: 1;');
+		jQuery('.section-app .carousel-item').eq(3).find('.carousel-content').attr('data--10p-top-top', 'opacity: 0;');
+		jQuery('.section-app .carousel-item').eq(3).find('.carousel-content').attr('data-anchor-target', '.carousel-item-4');
+
+		jQuery('.section-app .carousel-item').eq(3).find('img').attr('data--10p-bottom-bottom', 'display:block; opacity: 0; right: 0; top: 20vh;');
+		jQuery('.section-app .carousel-item').eq(3).find('img').attr('data--60p-bottom-bottom', 'opacity: 1; position: fixed; top: 10vh;');
+		jQuery('.section-app .carousel-item').eq(3).find('img').attr('data-0p-top-top', 'opacity: 1;');
+		jQuery('.section-app .carousel-item').eq(3).find('img').attr('data--10p-top-top', 'opacity: 0;');
+		jQuery('.section-app .carousel-item').eq(3).find('img').attr('data-anchor-target', '.carousel-item-4');
+
+		jQuery('.section-app .carousel-item').eq(4).find('.carousel-content').attr('data-0p-bottom-bottom', 'opacity: 0;');
+		jQuery('.section-app .carousel-item').eq(4).find('.carousel-content').attr('data--20p-bottom-bottom', 'opacity: 1;');
+		jQuery('.section-app .carousel-item').eq(4).find('.carousel-content').attr('data-0p-top-top', 'opacity: 1;');
+		jQuery('.section-app .carousel-item').eq(4).find('.carousel-content').attr('data--10p-top-top', 'opacity: 0;');
+		jQuery('.section-app .carousel-item').eq(4).find('.carousel-content').attr('data-anchor-target', '.carousel-item-5');
+
+		jQuery('.section-app .carousel-item').eq(4).find('img').attr('data--10p-bottom-bottom', 'display:block; opacity: 0; right: 0; top: 20vh;');
+		jQuery('.section-app .carousel-item').eq(4).find('img').attr('data--60p-bottom-bottom', 'opacity: 1; position: fixed; top: 10vh;');
+		jQuery('.section-app .carousel-item').eq(4).find('img').attr('data-0p-top-top', 'opacity: 1;');
+		jQuery('.section-app .carousel-item').eq(4).find('img').attr('data--10p-top-top', 'opacity: 0;');
+		jQuery('.section-app .carousel-item').eq(4).find('img').attr('data-anchor-target', '.carousel-item-5');
+
+		jQuery('.section-app .carousel-item').eq(5).find('.carousel-content').attr('data-0p-bottom-bottom', 'opacity: 0;');
+		jQuery('.section-app .carousel-item').eq(5).find('.carousel-content').attr('data--20p-bottom-bottom', 'opacity: 1;');
+		jQuery('.section-app .carousel-item').eq(5).find('.carousel-content').attr('data-0p-top-top', 'opacity: 1;');
+		jQuery('.section-app .carousel-item').eq(5).find('.carousel-content').attr('data--10p-top-top', 'opacity: 0;');
+		jQuery('.section-app .carousel-item').eq(5).find('.carousel-content').attr('data-anchor-target', '.carousel-item-6');
+
+		jQuery('.section-app .carousel-item').eq(5).find('img').attr('data--10p-bottom-bottom', 'display:block; opacity: 0; right: 0; top: 20vh;');
+		jQuery('.section-app .carousel-item').eq(5).find('img').attr('data--60p-bottom-bottom', 'opacity: 1; position: fixed; top: 10vh;');
+		jQuery('.section-app .carousel-item').eq(5).find('img').attr('data-0p-top-top', 'opacity: 1;');
+		jQuery('.section-app .carousel-item').eq(5).find('img').attr('data--10p-top-top', 'opacity: 0;');
+		jQuery('.section-app .carousel-item').eq(5).find('img').attr('data-anchor-target', '.carousel-item-6');
 	}
 
+	jQuery('.hero-background').attr('data-0p-bottom-bottom', 'opacity: 1;');
+	jQuery('.hero-background').attr('data--70p-bottom-bottom', 'opacity: 0;');
+	jQuery('.hero-background').attr('data-anchor-target', '.hero-content');
+
 	jQuery('.hero .hero-content').eq(0).find('a').attr('data-0p-bottom-bottom', 'opacity: 1; ');
-	jQuery('.hero .hero-content').eq(0).find('a').attr('data--50p-bottom-bottom', 'opacity: 0;');
+	jQuery('.hero .hero-content').eq(0).find('a').attr('data--90p-bottom-bottom', 'opacity: 0;');
 	jQuery('.hero .hero-content').eq(0).find('a').attr('data-anchor-target', '.hero-content');
 	jQuery('.hero .hero-content').eq(0).find('h2').attr('data-0p-bottom-bottom', 'opacity: 1; ');
-	jQuery('.hero .hero-content').eq(0).find('h2').attr('data--50p-bottom-bottom', 'opacity: 0;');
+	jQuery('.hero .hero-content').eq(0).find('h2').attr('data--90p-bottom-bottom', 'opacity: 0;');
 	jQuery('.hero .hero-content').eq(0).find('h2').attr('data-anchor-target', '.hero-content');
 	jQuery('.hero .hero-content').eq(0).find('p').attr('data-0p-bottom-bottom', 'opacity: 1; ');
-	jQuery('.hero .hero-content').eq(0).find('p').attr('data--50p-bottom-bottom', 'opacity: 0;');
+	jQuery('.hero .hero-content').eq(0).find('p').attr('data--90p-bottom-bottom', 'opacity: 0;');
 	jQuery('.hero .hero-content').eq(0).find('p').attr('data-anchor-target', '.hero-content');
 
+	jQuery('.hero .hero-arrow').attr('data-0p-bottom-bottom', 'opacity: 1; ');
+	jQuery('.hero .hero-arrow').attr('data--90p-bottom-bottom', 'opacity: 0;');
+	jQuery('.hero .hero-arrow').attr('data-anchor-target', '.hero-content');
 
 
+	if (window.matchMedia('(orientation: portrait)').matches) {
+		jQuery('.section-gallery').find('img').each(function() {
+			jQuery(this).attr('src', jQuery(this).attr('data-src'));
+		});
+	}
 
 	//--- END bind parallax data
 
@@ -158,17 +210,17 @@
 	});
 	//--- END parallax plugin
 
-	// // Everything that will be loaded for any resolution
-	// // Press accordion events
-	// 	jQuery('.accordion-picture').click(function() {
-	// 		var accordion = jQuery(this).parents('.accordion:first'),
-	// 			element = jQuery(this).parents('.accordion-item:first');
+	// Everything that will be loaded for any resolution
+	// Press accordion events
+		jQuery('.accordion-picture').click(function() {
+			var accordion = jQuery(this).parents('.accordion:first'),
+				element = jQuery(this).parents('.accordion-item:first');
 
-	// 		accordion.find('.accordion-target').text(element.text());
-	// 		accordion.find('.is-active').removeClass('is-active');
-	// 		element.addClass('is-active');
-	// 	});
-	// //--- END press accordion
+			accordion.find('.accordion-target').text(element.text());
+			accordion.find('.is-active').removeClass('is-active');
+			element.addClass('is-active');
+		});
+	//--- END press accordion
 
 	// Transitions
 	function onScroll(){
@@ -178,19 +230,30 @@
 			appSection = jQuery('.section-app'),
 			valveSection = jQuery('.section-valve'),
 			menu = jQuery('.menu-header'),
-			logo = jQuery('.logo'),
-			hero = jQuery('.hero');
+			logo = jQuery('.logo');
+			// hero = jQuery('.hero');
 
-		if(systemSection.position().top <= scrollPosition + 200 && systemSection.position().top + systemSection.outerHeight() > scrollPosition + 200) {
+		if(systemSection.position().top <= scrollPosition + 400 && systemSection.position().top + systemSection.outerHeight() > scrollPosition + 400) {
 			jQuery(systemSection.find('.carousel-item')).each(function(i) {
-				jQuery(this).addClass('animated slideInUp' + (i + 1));
+				jQuery(this).find('.carousel-image').addClass('animated slideInUp' + (i + 1));
+				jQuery(this).find('h3').addClass('animated fadeInUp' + (i + 1));
+				jQuery(this).find('p').addClass('animated fadeInUp' + (i + 1));
+				jQuery(systemSection.find('h2')).addClass('animated fadeInUp');
+
+				window.setTimeout(function(){
+					jQuery(systemSection.find('.carousel-item').eq(0)).addClass('animated');
+				}, 800);
+
+				window.setTimeout(function(){
+					jQuery(systemSection.find('.carousel-item').eq(1)).addClass('animated');
+				}, 1000);
 			});
-		} else if(sensorSection.position().top <= scrollPosition && sensorSection.position().top + sensorSection.outerHeight() > scrollPosition) {
+		} else if(sensorSection.position().top - 5 <= scrollPosition && sensorSection.position().top + sensorSection.outerHeight() > scrollPosition - 5) {
 			logo.addClass('white');
 			menu.addClass('dark');
 			menu.find('.button').addClass('button-alternate');
 			logo.removeClass('dark');
-		} else if (appSection.position().top <= scrollPosition && valveSection.position().top + valveSection.outerHeight() > scrollPosition) {
+		} else if (appSection.position().top - 5 <= scrollPosition && valveSection.position().top + valveSection.outerHeight() > scrollPosition - 5) {
 			logo.addClass('dark');
 			logo.removeClass('white');
 		} else {
@@ -200,13 +263,13 @@
 			menu.find('.button').removeClass('button-alternate');
 		}
 
-		if (window.matchMedia('(max-width: 47.5em)').matches) {
-			if(scrollPosition <= 0) {
-				hero.find('.hero-background').removeClass('blurred');
-			} else {
-				hero.find('.hero-background').addClass('blurred');
-			}
-		}
+		// if (window.matchMedia('(max-width: 47.5em)').matches) {
+		// 	if(scrollPosition <= 0) {
+		// 		hero.find('.hero-background').removeClass('blurred');
+		// 	} else {
+		// 		hero.find('.hero-background').addClass('blurred');
+		// 	}
+		// }
 	}
 	jQuery(document).on('scroll', onScroll);
 	//--- END transitions
